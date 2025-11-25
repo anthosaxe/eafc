4b6cacfa1bbc03f8791beb5c1a87fa9c153ded8b
Antho D <dhaene.anthony@hotmail.be>
2025-11-25 19:57:55 +0100

    dernier changement novembre

--- js/25_11.js
+++ js/25_11.js
@@ -1,3 +1,5 @@
+//exercice 1
+
 document.getElementById("btn").addEventListener("click", function () {
     const age = document.getElementById("age").value;
     const carte = document.getElementById("carte").checked;
@@ -25,4 +27,96 @@ document.getElementById("btn").addEventListener("click", function () {
     document.getElementById("resultat").innerHTML = "";
 
     document.getElementById("resultat").appendChild(div);
+});
+
+//exercice 2
+
+document.getElementById("btn2").addEventListener("click", function () {
+    const poids = document.getElementById("poids").value;
+    const zone = document.getElementById("zone").value;
+    let frais;
+    let prix_poids;
+
+    if (zone == "local") {
+        frais = 5;
+        prix_poids = poids * 2;
+    } else if (zone == "national") {
+        frais = 10;
+        if (poids < 10) {
+            prix_poids = poids * 4;
+        } else {
+            prix_poids = poids * 3;
+        }
+    } else {
+        frais = 15;
+        if (poids < 5) {
+            prix_poids = poids * 10;
+        } else {
+            prix_poids = poids * 7;
+        }
+    }
+
+    frais += prix_poids;
+
+    const div2 = document.createElement("div");
+    div2.className = "frais";
+    div2.textContent = `frais appliqué : ${frais} €`;
+
+    document.getElementById("resultat2").innerHTML = "";
+
+    document.getElementById("resultat2").appendChild(div2);
+});
+
+//exercice 3
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("btn3").addEventListener("click", function () {
+        const form = document.getElementById("commande");
+        const data = new FormData(form);
+
+        const commande = {
+            pizza: parseInt(data.get("pizza")) || 0,
+            pates: parseInt(data.get("pates")) || 0,
+            salade: parseInt(data.get("salade")) || 0,
+            dessert: parseInt(data.get("dessert")) || 0,
+            boisson: parseInt(data.get("boisson")) || 0,
+            carte: document.getElementById("carte_m").checked
+        }
+
+        console.log(commande)
+
+        prix_pizza = 10;
+        prix_pates = 8;
+        prix_salade = 6;
+        prix_dessert = 5;
+        prix_boisson = 2;
+
+        let prix = (commande.boisson * prix_boisson) + (commande.pizza * prix_pizza) + (commande.pates * prix_pates) + (commande.salade * prix_salade) + (commande.dessert * prix_dessert);
+        let nbre_plat = commande.boisson + commande.pizza + commande.pates + commande.salade + commande.dessert;
+        let carte = commande.carte;
+        let reduction = 0;
+
+        if (prix > 50) {
+            reduction += 10;
+        }
+
+        if (nbre_plat > 3) {
+            reduction += 5;
+        }
+
+        if (commande.carte) {
+            reduction += 10;
+        }
+
+        const prixFinal = prix * (1 - reduction / 100);
+
+        const div3 = document.createElement("div");
+        div3.className = "prix";
+        div3.textContent = `prix de la commande : ${prixFinal} €`;
+
+        document.getElementById("resultat3").innerHTML = "";
+
+        document.getElementById("resultat3").appendChild(div3);
+
+    });
 });
